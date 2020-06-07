ad26c7366c5a6d4bce41667be21f47e440bb2afd
Adrián S <56599033+asarabia-uala@users.noreply.github.com>
2020-06-07 14:59:06 -0300

    ok

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ const command = core.getInput('tf_actions_subcommand');
 
 switch (command) {
   case "init":
-    shell.exec('./terragrunt-init.js');
+    shell.exec('sudo node ./terragrunt-init.js');
     break;
   case "plan":
     shell.exec('sudo chmod +x ./tg-action/terragrunt-plan.js');
-    shell.exec('./tg-action/terragrunt-plan.js');
+    shell.exec('sudo node ./tg-action/terragrunt-plan.js');
     break;
 }
