975ed3f38769b605c20d8a0cae957abada3ffe76
Adrián S <56599033+asarabia-uala@users.noreply.github.com>
2020-06-11 01:00:13 -0300

    up

--- index.js
+++ index.js
@@ -10,12 +10,11 @@ try{
 
     console.log(tgplan);
 
-    //tgplan = tgplan.replace(/[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g,'');
-    //tgplan = tgplan.replace(/No changes. Infrastructure is up-to-date./g,"+ No changes. Infrastructure is up-to-date.");
- 
-    tgplan = tgplan.replace(/(?m)^ {2}\-/g,"-");
-    tgplan = tgplan.replace(/(?m)^ {2}\+/g,"+");
-    tgplan = tgplan.replace(/(?m)^ {2}\~/g,"~");
+    tgplan = tgplan.replace(/[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g,'');
+    tgplan = tgplan.replace(/No changes. Infrastructure is up-to-date./g,"+ No changes. Infrastructure is up-to-date.");
+    tgplan = tgplan.replace("Refreshing state...","");
+
+   
     tgplan = tgplan.replace("   -","-");
     tgplan = tgplan.replace("   +","+");
     tgplan = tgplan.replace("   ~","~");
