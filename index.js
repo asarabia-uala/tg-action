bbda4ba2b850bc9d4794284b8b5d78af856fb049
Adrián S <56599033+asarabia-uala@users.noreply.github.com>
2020-06-11 13:57:10 -0300

    up

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const core          = require('@actions/core');
-const github        = require('@actions/github');
-const child_process = require('child_process');
-import {runPlan} from '/utils/tgPlan.js';
+
+import {runPlan} from './utils/tgPlan.js';
 
 try{
       runPlan();
