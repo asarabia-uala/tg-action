d82d28d981762f2382e3ada352b2d3f365907bbf
Adrián S <56599033+asarabia-uala@users.noreply.github.com>
2020-06-12 15:23:27 -0300

    u

--- utils/comment_triggers.js
+++ utils/comment_triggers.js
@@ -1,7 +1,6 @@
 const { context, GitHub } = require("@actions/github");
 const tgplan    = require('./tgplan');
 
-
 const body = context.eventName === "issue_comment" ? context.payload.comment.body : context.payload.pull_request.body;
 
 if (context.eventName === "issue_comment" && !context.payload.issue.pull_request) {
