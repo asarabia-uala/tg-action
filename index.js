0d38c0640403403546efc0fb85df286faa897c47
Adrián S <56599033+asarabia-uala@users.noreply.github.com>
2020-06-12 17:34:54 -0300

    up

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const core      = require('@actions/core');
 const tgplan    = require('./utils/tgplan');
+const { context, GitHub } = require("@actions/github");
 
 try{
 
+    switch (context.eventName) {
+        case "issue_comment":
+            //
+            await client.reactions.createForIssueComment({
+                owner,
+                repo,
+                comment_id: context.payload.comment.id,
+                content: "eyes"
+            });
+            break;
+        case "@Apply":
+            //Apply
+            break;
+    }
 
-
-    tgplan.runPlan();
+        
+    //tgplan.runPlan();
     
 } catch (error) {
     core.setFailed(error.message);
