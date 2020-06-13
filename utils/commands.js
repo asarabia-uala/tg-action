8417490453a1b1f49a01022556ef8960d5be05b4
Adrián S <56599033+asarabia-uala@users.noreply.github.com>
2020-06-13 20:54:16 -0300

    up

--- utils/commands.js
+++ utils/commands.js
@@ -4,13 +4,14 @@ const utils         = require('./utils.js');
 
 function runCmd(command){
     const dir = core.getInput('path-to-hcl');
+    let cmdout;
 
     switch(command){
         case "plan":
-            let cmdout = child_process.execSync("terragrunt plan-all --terragrunt-non-interactive --terragrunt-source-update --terragrunt-include-external-dependencies",{encoding: "utf8", cwd: dir });
+            cmdout = child_process.execSync("terragrunt plan-all --terragrunt-non-interactive --terragrunt-source-update --terragrunt-include-external-dependencies",{encoding: "utf8", cwd: dir });
             break;
         case "apply":
-            let cmdout = child_process.execSync("terragrunt apply-all --terragrunt-non-interactive --terragrunt-include-external-dependencies",{encoding: "utf8", cwd: dir });
+            cmdout = child_process.execSync("terragrunt apply-all --terragrunt-non-interactive --terragrunt-include-external-dependencies",{encoding: "utf8", cwd: dir });
             break;
     }
 
