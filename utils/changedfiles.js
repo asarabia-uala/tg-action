8b1270670c7eef56f83481a43c868c4e768eaf1f
Adrián S <56599033+asarabia-uala@users.noreply.github.com>
2020-06-20 16:49:19 -0300

    ok

--- utils/changedfiles.js
+++ utils/changedfiles.js
@@ -71,7 +71,7 @@ async function changedFiles(path){
         .then(outputResults)
 		.catch(err => core.error(err) && (process.exitCode = 1));
 
-    let flag = chDirs.find( dir => dir == path );
+    let flag = chDirs.includes(path);
     console.log(flag);
 }
   
