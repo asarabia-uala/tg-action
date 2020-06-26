0ec2e1ef5c4ea1fdc9a60579509bc758a3fc8432
Adrián S <56599033+asarabia-uala@users.noreply.github.com>
2020-06-26 18:09:11 -0300

    up

--- utils/commands.js
+++ utils/commands.js
@@ -10,7 +10,7 @@ function runCmd(command){
             case "plan":
                 cmdout = child_process.execSync("terragrunt plan-all --terragrunt-non-interactive --terragrunt-source-update --terragrunt-include-external-dependencies -out tgplan.plan",{encoding: "utf8", cwd: dir });
                 child_process.execSync("zip tgplan.zip .terragrunt-cache/",{encoding: "utf8", cwd: dir });
-                ls = child_process.execSync("ls",{encoding: "utf8", cwd: dir });
+                ls = child_process.execSync("pwd",{encoding: "utf8", cwd: dir });
                 console.log(ls);
                 //utils.bucketPlan();
                 break;
