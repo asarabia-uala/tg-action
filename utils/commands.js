9f097dfc1959949c373ffaf50bbddc7699d212c3
Adrián S <56599033+asarabia-uala@users.noreply.github.com>
2020-06-29 16:38:14 -0300

    up

--- utils/commands.js
+++ utils/commands.js
@@ -14,13 +14,13 @@ async function runCmd(command){
             case "plan":
                 cmdout = child_process.execSync("terragrunt plan-all --terragrunt-non-interactive --terragrunt-source-update --terragrunt-include-external-dependencies -out tgplan.plan",{encoding: "utf8", cwd: dir });
                 child_process.execSync("zip tgplan.zip .terragrunt-cache/",{encoding: "utf8", cwd: dir });
-                await sleep(5000);
+                await sleep(9000);
                 utils.bucketPlan('up');
                 break;
             case "apply":
                 utils.bucketPlan();
 
-                await sleep(5000);
+                await sleep(9000);
                 
                 child_process.execSync("unzip tgplan.zip",{encoding: "utf8", cwd: dir });
                 // cmdout = child_process.execSync("terragrunt apply-all --terragrunt-non-interactive --terragrunt-include-external-dependencies",{encoding: "utf8", cwd: dir });
