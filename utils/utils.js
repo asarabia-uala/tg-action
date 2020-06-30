cef4a11582f2395dd7db1ac82baa7ceac9231136
Adrián S <56599033+asarabia-uala@users.noreply.github.com>
2020-06-30 01:12:22 -0300

    up

--- utils/utils.js
+++ utils/utils.js
@@ -69,14 +69,17 @@ function bucketPlan(method){
             }
         });
     }else{      
-        
-        const params = { Bucket: bucket, Key: key};
-
-        s3.getObject(params, (err, data) => {
-            if (err) console.error(err);
-            fs.writeFileSync(file, data.Body.toString());
-            console.log(`${file} has been created!`);
-          });
+        return new Promise(function(success, reject) {
+            s3.getObject({ Bucket: bucket, Key: key })
+            .createReadStream()
+            .pipe(fs.createWriteStream(file))
+            .on('close', function () {
+                console.log("sucesfully downloaded");
+            })
+            .on('error', function(err) {
+                console.log(err);
+            });
+        });
     }
 }
 
