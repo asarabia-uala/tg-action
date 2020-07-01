56d3dabefd54608a9dff782dcd094062567cb7a8
Adrián S <56599033+asarabia-uala@users.noreply.github.com>
2020-07-01 15:16:23 -0300

    ok

--- index.js
+++ index.js
@@ -7,28 +7,30 @@ const files     = require('./utils/changedfiles');
 try{
     const context = github.context;
     const path = core.getInput('path-to-hcl');
-    if(files.changedFiles(path)){
-        if(core.getInput('comment') == 'true'){
-            switch (context.eventName) {
-                case "pull_request_review":
-                    utils.ghComment("###  **[ Running Terragrunt Apply ... ]** :rocket:");
-                    break;
-                case "pull_request":
-                    utils.ghComment("###  **[ Running Terragrunt Plan ... ]**  :mag_right: \n\n ![](https://i.imgur.com/OhxcU6J.gif)");
-                    break;
-            }
-        }else{
+
+    console.log(files.changedFiles(path));
+    // if(files.changedFiles(path)){
+    //     if(core.getInput('comment') == 'true'){
+    //         switch (context.eventName) {
+    //             case "pull_request_review":
+    //                 utils.ghComment("###  **[ Running Terragrunt Apply ... ]** :rocket:");
+    //                 break;
+    //             case "pull_request":
+    //                 utils.ghComment("###  **[ Running Terragrunt Plan ... ]**  :mag_right: \n\n ![](https://i.imgur.com/OhxcU6J.gif)");
+    //                 break;
+    //         }
+    //     }else{
             
-                switch (context.eventName) {
-                    case "pull_request_review":
-                        command.runCmd("apply");
-                        break;
-                    case "pull_request":
-                        command.runCmd("plan");
-                        break;
-                }
-        }
-    }
+    //             switch (context.eventName) {
+    //                 case "pull_request_review":
+    //                     command.runCmd("apply");
+    //                     break;
+    //                 case "pull_request":
+    //                     command.runCmd("plan");
+    //                     break;
+    //             }
+    //     }
+    //}
             
 } catch (error) {
     core.setFailed(error.message);
