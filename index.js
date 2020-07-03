c6200aca3ee28c8040c94d24c732ced7d46fca6c
Adrián S <56599033+asarabia-uala@users.noreply.github.com>
2020-07-03 11:07:20 -0300

    up

--- index.js
+++ index.js
@@ -9,30 +9,29 @@ async function run (){
 try{
     const context = github.context;
     const path = core.getInput('path-to-hcl');
-    console.log(context);
-    console.log(await files.changedFiles(path));
-//     if(await files.changedFiles(path)){
-//         if(core.getInput('comment') == 'true'){
-//             switch (context.eventName) {
-//                 case "pull_request_review":
-//                     utils.ghComment("###  **[ Running Terragrunt Apply ... ]** :rocket:");
-//                     break;
-//                 case "pull_request":
-//                     utils.ghComment("###  **[ Running Terragrunt Plan ... ]**  :mag_right: \n\n ![](https://i.imgur.com/OhxcU6J.gif)");
-//                     break;
-//             }
-//         }else{
+
+    if(await files.changedFiles(path)){
+        if(core.getInput('comment') == 'true'){
+            switch (context.eventName) {
+                case "pull_request_review":
+                    utils.ghComment("###  **[ Running Terragrunt Apply ... ]** :rocket:");
+                    break;
+                case "pull_request":
+                    utils.ghComment("###  **[ Running Terragrunt Plan ... ]**  :mag_right: \n\n ![](https://i.imgur.com/OhxcU6J.gif)");
+                    break;
+            }
+        }else{
             
-//                 switch (context.eventName) {
-//                     case "pull_request_review":
-//                         command.runCmd("apply");
-//                         break;
-//                     case "pull_request":
-//                         command.runCmd("plan");
-//                         break;
-//                 }
-//         }
-//     }
+                switch (context.eventName) {
+                    case "pull_request_review":
+                        command.runCmd("apply");
+                        break;
+                    case "pull_request":
+                        command.runCmd("plan");
+                        break;
+                }
+        }
+    }
             
     } catch (error) {
         core.setFailed(error.message);
